98c34c7ced30c0eac821a282db502d2b17b68ac3
KevGary <kevgary.dev@gmail.com>
2015-11-12 15:07:12 -0700

    select by year route complete

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var pg = require('pg');
 var conString = "postgres://@localhost/memoriesapp";
 
-/* GET home page. */
+//GET ALL
 router.get('/api/v1/memories', function(req, res, next) {
  pg.connect(conString, function(err, client, done) {
    if (err) {
@@ -21,6 +21,7 @@ router.get('/api/v1/memories', function(req, res, next) {
  });
 });
 
+//INSERT ONE
 router.post('/api/v1/memories', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
@@ -37,5 +38,27 @@ router.post('/api/v1/memories', function(req, res, next) {
     });
   });
 })
+
+//GET ALL/:YEAR
+router.get('/api/v1/memories/:id', function(req ,res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    console.log(req.params)
+    client.query('SELECT * FROM memories WHERE year = $1', ['1994'], function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+       res.send(result);
+    });
+  });
+})
+
+router.get('')
+
+
 
 module.exports = router;
